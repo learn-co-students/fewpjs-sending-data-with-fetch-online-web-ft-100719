64df764c470bbd420e3fe4fd43fcefbea5749257
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-01-20 12:18:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your code here
+const b = document.querySelector("body");
+
+let formData = (name, email) => {
+  return {
+    name: name,
+    email: email
+  };
+};
+
+let configObj = (name, email) => {
+  return {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(formData(name, email))
+  };
+};
+
+const submitData = (name, email) => {
+  return fetch("http://localhost:3000/users", configObj(name,email))
+  .then(parseJSON)
+  .then(renderId)
+  .catch(function(error) {
+    b.append("Unauthorized Access")
+    console.log(error.message);
+  });
+};
+
+const renderId = (json) => {
+  b.append(json.id)
+}
+
+const parseJSON = (response) => {
+  return response.json();
+}
