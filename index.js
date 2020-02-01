2d6246e1ad70840434626223a02c944f67eeae98
Emilia <emilia.mena26@gmail.com>
2020-02-01 15:47:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name: "Lucky",
+                email: "lucky@thecodebusters.com"
+            })
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            document.body.innerHTML = object["id"]
+        })
+        .catch(function(error) {
+            alert("Houston, We have a problem. CALL THE CODEBUSTERS!");
+            document.body.innerHTML = error.message
+        })
+};
