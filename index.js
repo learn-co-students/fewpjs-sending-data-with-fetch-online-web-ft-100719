bc51896688f23b4e6c71bc7f2f6eba2f878b95e2
Stefanie <stefcodes@gmail.com>
2020-02-01 17:08:04 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// Add your code here
+ // Add your code here
+// let submitData = {
+//     userName: "Tom",
+//     userEmail: "Tommy@gmail.com"
+//   };
+
+function submitData (name, email) {
+    let userInfo = {
+        name: name,
+        email: email
+    }
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userInfo)
+    }
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(object) {
+        document.body.innerHTML = object["id"]
+    })
+    .catch(function(error) {
+        document.body.innerHTML = error.message
+    })
+}
+
+submitData()
+
+
+  // method: "POST" is missing from the object below
+    // let formData = {
+    //     dogName: "Byron",
+    //     dogBreed: "Poodle"
+    //   };
+       
+    //   let configObj = {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       "Accept": "application/json"
+    //     },
+    //     body: JSON.stringify(formData)
+    //   };
+       
+    //   fetch("http://localhost:3000/dogs", configObj)
+    //     .then(function(response) {
+    //       return response.json();
+    //     })
+    //     .then(function(object) {
+    //       console.log(object);
+    //     });
